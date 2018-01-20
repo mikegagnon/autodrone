301866b472b34d6689a1e565146f138e40fb23c6
Michael Gagnon <mikegagnon@gmail.com>
2018-01-19 18:54:35 -0800

    Remove commented out code

--- src/main/scala/me/michaelgagnon/chopper/Camera.scala
+++ src/main/scala/me/michaelgagnon/chopper/Camera.scala
@@ -20,25 +20,4 @@ class Camera(val canvasSize: Xy) {
   // Convert level coordinate to canvas coordinate
   def toCanvas(levelCoordinate: Xy) = Xy(levelCoordinate.x - x, levelCoordinate.y - y)
 
-/*
-  def placeVisual(vle: VisualLevelElement) =
-    vle.setXy(XyConst(vle.levelElement.x - x, vle.levelElement.y - y))
-
-  def placeBitmapBackground(ble: BitmapLevelElement) {
-    ble.bitmap.x = ble.levelElement.x - x * 0.2
-    // TODO: undo 390 const
-    ble.bitmap.y = ble.levelElement.y - y * 0.2 + 390
-  }
-
-  def update(droneBitmap: XyConst) {
-    if (droneBitmap.x > Camera.rightBorder) {
-      x += droneBitmap.x - Camera.rightBorder
-    } else if (droneBitmap.x < Camera.leftBorder) {
-      x -= Camera.leftBorder - droneBitmap.x
-    }
-
-    x = Math.max(x, 0)
-    x = Math.min(x, Level.width - Config.canvasWidth)
-  }
-*/
 }
