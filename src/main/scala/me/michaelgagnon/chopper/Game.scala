74350994d7bdb1df400246ea73aab0f9d6876c06
Michael Gagnon <mikegagnon@gmail.com>
2018-01-20 09:40:21 -0800

    Whitespace

--- src/main/scala/me/michaelgagnon/chopper/Game.scala
+++ src/main/scala/me/michaelgagnon/chopper/Game.scala
@@ -5,7 +5,6 @@ import org.querki.jquery._
 class Game(val viz: Viz, val level: Level) {
 
   val droneBitmap: VizElement = viz.getDroneVizElement(level)
-
   val vizElements: Seq[VizElement] = viz.getVizElements(level)
 
 }
