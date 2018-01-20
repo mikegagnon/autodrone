33344e9995f060c6c3fc33ec25bf2b5a169dff15
Michael Gagnon <mikegagnon@gmail.com>
2018-01-19 18:17:07 -0800

    Element.scala

--- src/main/scala/me/michaelgagnon/chopper/Element.scala
+++ src/main/scala/me/michaelgagnon/chopper/Element.scala
@@ -1,8 +1,22 @@
 package me.michaelgagnon.chopper
 
-sealed abstract class Element {
-  val x: Double
-  val y: Double
-  val width: Double
-  val height: Double
+abstract class Element(val origPosition: Xy) {
+  val currentPosition: Xy = Xy(origPosition.x, origPosition.y)
+  val dim: Xy
+}
+
+object GroundElement {
+  val dim = Xy(32.0, 32.0)
+}
+
+case class GroundElement(override val origPosition: Xy) extends Element(origPosition) {
+  val dim = GroundElement.dim
+}
+
+object FireElement {
+  val dim = Xy(50.0, 100.0)
+}
+
+case class FireElement(override val origPosition: Xy) extends Element(origPosition) {
+  val dim = FireElement.dim
 }
--- src/main/scala/me/michaelgagnon/chopper/Level.scala
+++ src/main/scala/me/michaelgagnon/chopper/Level.scala
@@ -9,5 +9,10 @@ sealed trait Level {
 class Level1 extends Level {
   val dim = Xy(100, 200)
   val numBackgrounds = 2
-  val elements = Seq[Element]()
+  val elements = Seq[Element](
+    //WaterElement(250.0, -400),
+    FireElement(Xy(50.0,  -FireElement.dim.y - GroundElement.dim.y)),
+    FireElement(Xy(100.0, -FireElement.dim.y - GroundElement.dim.y)),
+    FireElement(Xy(150.0, -FireElement.dim.y - GroundElement.dim.y))
+  )
 }
