9d20b4d90b1ada62e5501a17b67a9befc914014f
Michael Gagnon <mikegagnon@gmail.com>
2018-01-19 16:15:39 -0800

    determine canvasSize

--- src/main/scala/me/michaelgagnon/chopper/Game.scala
+++ src/main/scala/me/michaelgagnon/chopper/Game.scala
@@ -1,5 +1,19 @@
 package me.michaelgagnon.chopper
 
+import org.querki.jquery._
+
 class Game(id: String) {
+
+  val div = $(s"#$id")
+
+  val canvasId = id + "canvas"
+
+  val canvas = div.find("canvas")
+
+  canvas.attr("id", canvasId)
+
+  val canvasSize = Xy(
+    canvas.attr("width").toOption.get.toDouble,
+    canvas.attr("height").toOption.get.toDouble)
 
 }
---
+++ src/main/scala/me/michaelgagnon/chopper/Xy.scala
@@ -0,0 +1,3 @@
+package me.michaelgagnon.chopper
+
+case class Xy(var x: Double, var y: Double)
