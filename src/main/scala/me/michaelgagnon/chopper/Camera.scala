93f8b7e2cab604330cc1989113410574bbc3adc6
Michael Gagnon <mikegagnon@gmail.com>
2018-01-21 15:18:24 -0800

    Background vertical scrolling

--- src/main/scala/me/michaelgagnon/chopper/Camera.scala
+++ src/main/scala/me/michaelgagnon/chopper/Camera.scala
@@ -26,7 +26,7 @@ class Camera(val canvasSize: Xy, val levelDim: Xy) {
 
   def placeBackground(background: Background) {
     background.bitmap.x = background.index * background.dim.x - (x * Background.scalingFactor)
-    background.bitmap.y = 0//-background.dim.y
+    background.bitmap.y = -background.dim.y - y
   }
 
   def positionCamera(droneVizElement: VizElement[DroneElement]) {
