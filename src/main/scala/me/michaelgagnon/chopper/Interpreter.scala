f863334dcdeda0220d0487e66c3a3bb27e85f417
Michael Gagnon <mikegagnon@gmail.com>
2018-01-25 12:21:55 -0800

    executeAssignment

--- src/main/scala/me/michaelgagnon/chopper/Interpreter.scala
+++ src/main/scala/me/michaelgagnon/chopper/Interpreter.scala
@@ -3,15 +3,10 @@ package me.michaelgagnon.chopper
 import scala.collection.mutable
 import ChopperParser._
 
+// TODO: do we really need to store id here?
 case class Variable(id: String, typ: MeasurementUnitType, var value: Double)
 
 case class InterpreterCrash(private val message: String) extends Exception(message) 
-
-/*
-sealed trait InterpreterResult
-case class Error(msg: String) extends InterpreterResult
-case object Success extends InterpreterResult
-*/
 
 class State {
   val variables = new mutable.HashMap[String, Variable]()
@@ -32,22 +27,38 @@ class Interpreter(val state: State) {
         throw new InterpreterCrash(s"Cannot assign ${id} to ${destVariable.id}, ${id} is undefined")
     }
 
-  def updateVariable(variable: Variable, value: Value) =
+  def updateVariableFromLiteral(destVariable: Variable, v: Double, measurementUnit: MeasurementUnitType) {
+    if (destVariable.typ == measurementUnit) {
+      destVariable.value = v
+    } else {
+      throw new InterpreterCrash(s"Cannot assign ${v} to ${destVariable.id}, because their types do not match")
+    }
+  }
+
+  def updateVariable(destVariable: Variable, value: Value) =
     value match {
-      case IDENTIFIER(id) => updateVariableFromIdentifier(variable, id)
-      case DoubleWithType(v, measurementUnit) => ()
+      case IDENTIFIER(id) => updateVariableFromIdentifier(destVariable, id)
+      case DoubleWithType(v, measurementUnit) => updateVariableFromLiteral(destVariable, v, measurementUnit.unit)
     }
 
-  def newVariable(id: IDENTIFIER, value: Value) {
 
-  }
+  def newVariableFromIdentifier(destId: String, srcId: String) =
+    state.variables.get(srcId) match {
+      case Some(Variable(_, measurementUnit, v)) => state.variables(destId) = Variable(destId, measurementUnit, v)
+      case None => throw new InterpreterCrash(s"Cannot assign ${srcId} to ${destId} because ${destId} is not defined")
+    }
+
+  def newVariable(destId: IDENTIFIER, value: Value) =
+    value match {
+      case IDENTIFIER(srcId) => newVariableFromIdentifier(destId.id, srcId)
+      case DoubleWithType(v, measurementUnit) => state.variables(destId.id) = Variable(destId.id, measurementUnit.unit, v)
+    }
 
   def executeAssignment(id: IDENTIFIER, value: Value) =
     state.variables.get(id.id) match {
       case Some(variable) => updateVariable(variable, value)
       case None => newVariable(id, value)
     }
-
 
   def executeStatement(statement: ChopperAst) =
     statement match {
